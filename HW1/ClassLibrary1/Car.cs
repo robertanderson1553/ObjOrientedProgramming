c3c0b2f66f6f3f787804fd4d18e77cfcae830ed3
Robert Anderson <35211428+robertanderson1553@users.noreply.github.com>
2018-01-18 18:48:43 -0800

    HW! Create Car Class
    Create a visual studio solution that has both a console project and a class library project. I defined a class in the class library, and then use that class to create 2 objects in the console program.

---
+++ HW1/ClassLibrary1/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Car
+    {
+        private string _Color;
+        private int _Weight;
+
+        //Default constructor
+        public Car()
+        {
+            
+        }
+
+        //Constructor for passing in parameters for color and weight
+        public Car(string pColor, int pWeight)
+        {
+            this.Color = pColor;
+            this.Weight = pWeight;
+        }
+
+        public string Color
+        {
+            get { return _Color; }
+            set { _Color = value; }
+        }
+
+        public int Weight
+        {
+            get { return _Weight; }
+            set { _Weight = value; }
+        }
+
+        public string Details()
+        {
+            return "This car weighs " + _Weight + " pounds and it is " + _Color + ". ";
+        }
+    }
+}
---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar1 = new Car();
+            Car myCar2 = new Car();
+
+            myCar1.Color = "Blue";
+            myCar1.Weight = 3000;
+            Console.WriteLine("{0}", myCar1.Details());
+            
+            myCar2.Color = "Red";
+            myCar2.Weight = 4000;
+            Console.WriteLine("{0}", myCar2.Details());
+
+            Console.ReadLine();
+        }
+    }
+}
